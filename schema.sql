6bc727439d337d027ae2ee389e8c686ef976f94d
Sherreka <rekadarnb@gmail.com>
2022-05-15 11:34:15 -0400

    added json info, dependencies, and schema

---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department(
+   id INT AUTO_INCREMENT,
+   name VARCHAR(30) NOT NULL,
+   PRIMARY KEY(id)
+);
+
+CREATE TABLE role(
+   id INT AUTO_INCREMENT,
+   title VARCHAR(30) NOT NULL,
+   salary DECIMAL(7,2),
+   department_id INT NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY(role_id) REFERENCES role(id)
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
