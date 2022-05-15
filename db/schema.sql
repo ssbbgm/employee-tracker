69ec1e25237d28176e851d0711257b01c4cea2a4
Sherreka <rekadarnb@gmail.com>
2022-05-15 14:19:08 -0400

    added role info

--- db/schema.sql
+++ db/schema.sql
@@ -3,33 +3,34 @@ DROP DATABASE IF EXISTS company_db;
 -- Creating the database 
 CREATE DATABASE company_db;
 
---Using this database
+-- Using this database
 USE company_db;
 
---Creating the department table
+-- Creating the department table
 CREATE TABLE department(
    id INT AUTO_INCREMENT,
    name VARCHAR(30) NOT NULL,
-   PRIMARY KEY(id)
+   PRIMARY KEY (id)
 );
 
---Creating the role table
+-- Creating the role table
 CREATE TABLE role(
    id INT AUTO_INCREMENT,
    title VARCHAR(30) NOT NULL,
    salary DECIMAL(7,2),
-   department_id INT NOT NULL,
-   PRIMARY KEY(id),
+   department_id INT,
+   PRIMARY KEY (id),
    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
---Creating the employee table
+-- Creating the employee table
 CREATE TABLE employee(
-    id INT AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT,
-    FOREIGN KEY(role_id) REFERENCES role(id)
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+   id INT AUTO_INCREMENT,
+   first_name VARCHAR(30) NOT NULL,
+   last_name VARCHAR(30) NOT NULL,
+   role_id INT,
+   manager_id INT,
+   PRIMARY KEY(id),
+   FOREIGN KEY (role_id) REFERENCES role(id),
+   FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
----Insert department info
+-- Insert department info
 INSERT INTO department (name)
 VALUE ('Sales');
 
@@ -11,7 +11,22 @@ VALUE ('Finance');
 INSERT INTO department (name)
 VALUE ('Legal');
 
-
 -- Insert role info
+
+INSERT INTO role(title, salary, department_id)
+VALUE('Sales Associate', 40000, 1),
+('Sales Lead', 60000, 1), 
+('Entry Software Engineer', 80,000, 2),
+('Mid Software Engineer', 120,000, 2),
+('Lead Software Engineer', 180,000, 2),
+('Financial Administrator', 60,000, 3),
+('Accountant', 100,000, 3),
+('Finance Manager', 130,000, 3),
+('Chief Legal Officer', 250,000, 4),
+('Legal Assitant', 60,000, 4),
+('Contracts Administrator', 90,000, 4),
+
+
+
 
 -- Insert employee info
