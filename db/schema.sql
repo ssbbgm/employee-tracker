261b23639a8397339622baa51d0d6389130dbc03
Sherreka <rekadarnb@gmail.com>
2022-05-16 17:13:25 -0400

    added updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -6,31 +6,30 @@ CREATE DATABASE company_db;
 -- Using this database
 USE company_db;
 
--- Creating the department table
-CREATE TABLE department(
-   id INT AUTO_INCREMENT,
-   name VARCHAR(30) NOT NULL,
-   PRIMARY KEY (id);
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
 );
 
--- Creating the role table
-CREATE TABLE role(
-   id INT AUTO_INCREMENT,
-   title VARCHAR(30) NOT NULL,
-   salary DECIMAL(7,2),
-   department_id INT,
-   PRIMARY KEY (id),
-   FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE;
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
--- Creating the employee table
-CREATE TABLE employee(
-   id INT AUTO_INCREMENT,
-   first_name VARCHAR(30) NOT NULL,
-   last_name VARCHAR(30) NOT NULL,
-   role_id INT,
-   manager_id INT,
-   PRIMARY KEY(id),
-   FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-   FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE;
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED
 );
+
+
+-- INDEX man_ind (manager_id),
+-- CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
--- db/seed.sql
+++ db/seed.sql
@@ -1,30 +1,24 @@
 -- Insert department info
 INSERT INTO department (name)
-VALUE ('Sales');
-
-INSERT INTO department (name)
-VALUE ('Development');
-
-INSERT INTO department (name)
-VALUE ('Finance');
-
-INSERT INTO department (name)
-VALUE ('Legal');
+VALUE ('Sales'), 
+('Development'),
+('Finance'),
+('Legal');
 
 -- Insert role info
 
 INSERT INTO role(title, salary, department_id)
 VALUE('Sales Associate', 40000, 1),
 ('Sales Lead', 60000, 1), 
-('Entry Software Engineer', 80,000, 2),
-('Mid Software Engineer', 120,000, 2),
-('Lead Software Engineer', 180,000, 2),
-('Financial Administrator', 60,000, 3),
-('Accountant', 100,000, 3),
-('Finance Manager', 130,000, 3),
-('Chief Legal Officer', 250,000, 4),
-('Legal Assistant', 60,000, 4),
-('Contracts Administrator', 90,000, 4);
+('Entry Software Engineer', 80000, 2),
+('Mid Software Engineer', 120000, 2),
+('Lead Software Engineer', 180000, 2),
+('Financial Administrator', 60000, 3),
+('Accountant', 100000, 3),
+('Finance Manager', 130000, 3),
+('Chief Legal Officer', 250000, 4),
+('Legal Assistant', 60000, 4),
+('Contracts Administrator', 90000, 4);
 
 -- Insert employee info
 
