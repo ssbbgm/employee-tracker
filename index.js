c8ae9101d7435a6d676fc1b05ba4112162266939
Sherreka <rekadarnb@gmail.com>
2022-05-16 17:36:32 -0400

    added main menu

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+
 // Import and require mysql2
 const mysql = require('mysql2');
 // Import and require inquirer
 const inquirer = require('inquirer');
 // Import and require console.table
-const inquirer = require('console.table');
+const cTable = require('console.table');
 
 
 
@@ -20,3 +20,29 @@ const connection = mysql.createConnection(
   },
   console.log(`Connected to the company_db database.`)
 );
+
+//Connect to inquirer main questions
+connection.connect(function (err) {
+    if (err) throw err;
+    startMenu();
+  });
+
+
+const startMenu = () => {
+    inquirer
+    .prompt({
+      name: "menu",
+      type: "list",
+      message: "Welcome to the menu!",
+      choices: [
+        "View all employees",
+        "View all employees by role",
+        "View all employees by department",
+        "View all employees by manager",
+        "Add employee",
+        "Add role",
+        "Add department",
+        "Update employee role",
+      ]
+    })
+}
